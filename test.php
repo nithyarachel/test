ad0824cd50c72f73c429596009ad831b8768f763
nithyarachel <nithyarachel@gmail.com>
2014-07-25 16:01:06 +0530

    Update test.php

--- test.php
+++ test.php
@@ -8,22 +8,7 @@
     $_M['admin_view_info'] = 'Admin viewed info about cert. Remote id:%s';
     $_M['admin_view_info_for_non_configured_cert'] = 'Admin viewed info about non-configured cert';
     
-    $_M['admin_sync_updated_duedate'] = 'Due date updated from admin area. Due date:%s';
-    $_M['admin_sync_updated_domain'] = 'Domain updated from admin area. Domain:%s';
-    $_M['admin_sync_updated_remoteid'] = 'Remote id updated from admin area. Old id:%s, current id:%s';
-    
-
-    $_M['admin_resend_configuration_email'] = 'Admin sent configuration email';    
-    
-    $_M['admin_create'] = 'Admin created certificate on whmcs side. Now client has to configure certificate';
-    $_M['client_step_one'] = 'Client visited 1th configuration page. Certificate type:%s';
-    $_M['client_step_two'] = 'Client visited 2th configuration page. Certificate type:%s, domain:%s';
-    $_M['client_step_three_remote_id_selected_from_account'] = '3th configuration page: Certificate type:%s, remoted id %s has been recieved from NC account (used tokens)';
-    $_M['client_step_three_remote_id_created'] = '3rd configuration page: Certificate type:%s, certificate has been created in NC account by module. Remote id:%s';
-    
-    $_M['client_step_three_activated'] = '3rd configuration page: certificate has been activated. Certificate type:%s, remote id:%s, approver email:%s, admin email:%s';
-    $_M['client_step_three_activated_http_based_validation'] = '3rd configuration page: certificate has been activated. Certificate type:%s, remote id:%s, http-based validation, admin email:%s';
-    
+   
     $_M['client_view_details'] = 'Client viewed details';
     $_M['client_resend_approver_email'] = 'Client sent approver email';
     $_M['client_resend_cert'] = 'Client sent cert';
